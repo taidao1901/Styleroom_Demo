7bd85d65fe3b931182de45d379e101c0405913c7
taidao1901 <19522148@gm.uit.edu.vn>
2023-10-19 11:49:48 +0700

    update version

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import streamlit as st
 st.set_page_config(layout="wide",initial_sidebar_state='collapsed')
 def main():
     st.title("Welcome to Styleroom!!")
+    st.write("version: 0.0.1")
 if __name__ == "__main__":
     main()
