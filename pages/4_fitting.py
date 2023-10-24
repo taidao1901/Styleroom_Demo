0c05d60d2ba83e379d0127a45fae49b60594c35a
taidao1901 <19522148@gm.uit.edu.vn>
2023-10-24 10:20:28 +0700

    remove pyc

---
+++ pages/4_fitting.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from utilities.fitting import components
+st.set_page_config(layout="wide",initial_sidebar_state='collapsed')
+def main():
+    left, mid, right  = st.columns([1,1,1])
+    with mid:
+        re
+        components.show_image()
---
+++ utilities/fitting/components.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def show_image(image, width:int):
+    st.image(image, width=width)
